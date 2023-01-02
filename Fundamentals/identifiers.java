5062406eacf83135c7494230df0a59996c45d09f
lakhan pal <lakhanpal185@gmail.com>
2023-01-02 22:19:02 +0530

    identifiers file added with numbers datatypes

---
+++ Fundamentals/identifiers.java
@@ -0,0 +1,16 @@
+class identifiers{ //class name ,variable name , method name are indentifiers
+    public static void main(String [] args){
+        int intvar = 10;   //32bit
+        byte bytevar = 39; //8bit 
+        short shortvar = 78; //16bit
+        long longvar = 97L; // 8bytes
+
+        System.out.println("intvar : "+ intvar );
+        System.out.println("bytevar : "+ bytevar );
+        System.out.println("shortvar : "+ shortvar );
+        System.out.println("longvar : "+ longvar );
+
+        float floatvar = 89.90f;// 32bit
+        double doublevar = 76.8; //8 bytes
+    }
+}
