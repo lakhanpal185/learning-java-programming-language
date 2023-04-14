932ac86f6a39ef470b4c5d7479c4dd9383d659ae
lakhan pal <lakhanpal185@gmail.com>
2023-04-14 07:33:23 +0530

    package in java

---
+++ ControlFlow/packages.java
@@ -0,0 +1,19 @@
+package com.mycode.game;
+//com.icicibanck.load.Account;
+//domainname.modeulename.submodulename.classname;
+//that's how we can write packeges there must be only one
+
+public class packages{
+    public static void main(String[] args){
+        System.out.println("hey i am doing well");
+    }
+}
+
+// java source file structer
+// package statement;
+// import statemenets;
+// class /interfaces/ enum declarations
+
+//javac -d [] filename.java
+//  d stands for destination folder where class should be generated
+// this file contains errors try it!
