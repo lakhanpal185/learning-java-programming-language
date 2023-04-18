3258ec600b718e3188dbe645c4728751ba2c490e
lakhan pal <lakhanpal185@gmail.com>
2023-04-18 22:49:47 +0530

    strictfp and abstract in java

---
+++ AccessModifiers/strictfp.java
@@ -0,0 +1,12 @@
+//applicable on class and methods
+//methods that are declared as strictfp all floatingpoint calculations 
+//in that method has to follow ieee 754 standard
+//strictfp -abstract is valid for class but not for methods becoz abstract does
+//not provide implementation
+abstract strictfp class{
+
+}
+// this is valid
+
+// public abstract strictfp void mystore(); 
+//invalid
