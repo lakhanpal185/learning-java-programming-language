719f45123784ba4ba5a48cfa67dfba9a401fbeb5
lakhan pal <lakhanpal185@gmail.com>
2023-04-25 06:38:04 +0530

    interface methods and variable description

--- AccessModifiers/interface/interfaceDetails.java
+++ AccessModifiers/interface/interfaceDetails.java
@@ -7,7 +7,9 @@ interface interf{
 }
 abstract class interfaceDetails implements interf{
         // if abstract is not then we have to provide implementation for all methods
-
+        public static void main(String [] args){
+            System.out.println("going to use interface methods BUT NOT ");
+        }
         public void ma1(){ // this must be public other wise we will get an error
 
         }
@@ -26,3 +28,18 @@ interface B{
 interface AB extends A,B{
     
 }
+
+// some points about the interface variables and methods
+// variables : the variable are by default public static final weather we are declaring or not
+
+interface interf{
+    int x = 0; // this is public static final int x
+}
+//  static : the implenetation class can use it without the object
+// public : make it available for all the implementing class
+// final : make it unchangable by any method
+// must be initialize at the time of declaraton
+
+// methods : methods are by default public abstract void 
+//          this make it available to implementation class
+
