e261fc3520afbec9384ce30e0ba0719f42dd5f7b
lakhan pal <lakhanpal185@gmail.com>
2023-04-26 17:55:26 +0530

    marker interface in java

---
+++ AccessModifiers/interface/markerInterface.java
@@ -0,0 +1,9 @@
+// The marker interface in java is an wont contains methods, implementing these interfaces our 
+// obj get ability such type of such type of interface are called marker interfase/tag interface ability interface
+
+// serializable, cloneable, RandomAccess, etc
+//  by implementing serializable our object get ability to send and receive data from the n/w and we can save
+//  state of object into the file
+// clonable provide to make clone object
+// this is functionality is develop by jvm we can create their own but we hava to make changes in jvm
+
